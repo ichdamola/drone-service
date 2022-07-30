8f6c5434d3cbeaa5b594f4f9d06bfa0b7742bebb
Ichdamola <adedadamola@gmail.com>
2022-07-30 09:41:34 +0100

    [CRTE] create the Medication schema object.

--- src/main/java/com/drone/services/models/Medication.java
+++ src/main/java/com/drone/services/models/Medication.java
@@ -1,2 +1,20 @@
-package com.drone.services.models;public class Medication {
+package com.drone.services.models;
+
+import javax.persistence.*;
+
+@Entity
+public class Medication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private Integer weight;
+    private String code;
+    private String image;
+
+    @ManyToOne
+    @JoinColumn(name = "drone_id")
+    private Drone drone;
+
+
 }
