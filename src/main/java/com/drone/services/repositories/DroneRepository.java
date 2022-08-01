c7f628f0a62d3152172336b0485c5cd311d4a7fe
Ichdamola <adedadamola@gmail.com>
2022-08-01 09:52:36 +0100

    [CRTE] create Drone model repositiry.

--- src/main/java/com/drone/services/repositories/DroneRepository.java
+++ src/main/java/com/drone/services/repositories/DroneRepository.java
@@ -1,2 +1,9 @@
-package com.drone.services.repositories;public interface DroneRepository {
+package com.drone.services.repositories;
+
+import com.drone.services.models.Drone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DroneRepository extends JpaRepository<Drone, Integer> {
 }
