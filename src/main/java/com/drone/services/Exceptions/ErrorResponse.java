cb14c28a0aec587d84c46a2f725b72f02695092a
Ichdamola <adedadamola@gmail.com>
2022-08-01 09:54:11 +0100

    [CRTE] create Error response file.

--- src/main/java/com/drone/services/Exceptions/ErrorResponse.java
+++ src/main/java/com/drone/services/Exceptions/ErrorResponse.java
@@ -1,2 +1,24 @@
-package com.drone.services.Exceptions;public class ErrorResponse {
+package com.drone.services.Exceptions;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+    private int status;
+    private String message;
+    private Map<String, String> errors;
+
+    public ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public ErrorResponse(int status, Map<String, String> errors) {
+        this.status = status;
+        this.errors = errors;
+    }
 }
