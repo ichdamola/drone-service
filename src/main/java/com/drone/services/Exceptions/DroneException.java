aa47d6285f3ac08c7784ecd27d0812b34cd4bcc3
Ichdamola <adedadamola@gmail.com>
2022-08-01 09:55:15 +0100

    [CRTE] create custom error messages

--- src/main/java/com/drone/services/Exceptions/DroneException.java
+++ src/main/java/com/drone/services/Exceptions/DroneException.java
@@ -1,2 +1,18 @@
-package com.drone.services.Exceptions;public class DroneWeightException {
+package com.drone.services.Exceptions;
+
+public class DroneException extends RuntimeException{
+    public DroneException() {
+    }
+
+    public DroneException(String message) {
+        super(message);
+    }
+
+    public DroneException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DroneException(Throwable cause) {
+        super(cause);
+    }
 }
