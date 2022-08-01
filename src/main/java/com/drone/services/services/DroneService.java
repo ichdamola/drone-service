4c683afebffe4f0416c147d0d21088e4cc291f2e
Ichdamola <adedadamola@gmail.com>
2022-08-01 09:59:52 +0100

    [CRTE] create service interface for the Drone method

--- src/main/java/com/drone/services/services/DroneService.java
+++ src/main/java/com/drone/services/services/DroneService.java
@@ -1,2 +1,9 @@
-package com.drone.services.services;public interface DroneService {
+package com.drone.services.services;
+
+import com.drone.services.dtos.DroneDTO;
+import com.drone.services.models.Drone;
+
+public interface DroneService {
+    Drone registerDrone(DroneDTO newDrone);
+
 }
