a24da194d2c7f312dd623cd5d4e7850788c72dd0
Ichdamola <adedadamola@gmail.com>
2022-08-01 10:00:38 +0100

    [CRTE] Drone object implementation object.

--- src/main/java/com/drone/services/services/DroneServiceImpl.java
+++ src/main/java/com/drone/services/services/DroneServiceImpl.java
@@ -1,2 +1,25 @@
-package com.drone.services.services;public class DroneServiceImpl {
+package com.drone.services.services;
+
+import com.drone.services.dtos.DroneDTO;
+import com.drone.services.models.Drone;
+import com.drone.services.repositories.DroneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DroneServiceImpl implements DroneService {
+    private final DroneRepository droneRepository;
+
+    @Autowired
+    public DroneServiceImpl (DroneRepository droneRepository){
+        this.droneRepository = droneRepository;
+    }
+
+    @Override
+    public Drone registerDrone(DroneDTO newDrone) {
+        Drone drone = new Drone(newDrone.getModel(), newDrone.getSerialNumber(), newDrone.getWeight(), newDrone.getBatteryCapacity());
+        return droneRepository.save(drone);
+    }
+
+
 }
