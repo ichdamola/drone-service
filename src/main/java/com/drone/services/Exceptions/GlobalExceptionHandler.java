4a1cb9e619dd90fa64c57498acb067d6fe9ace34
Ichdamola <adedadamola@gmail.com>
2022-08-01 09:53:36 +0100

    [CRTE] create Global exception handler file.

--- src/main/java/com/drone/services/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/drone/services/Exceptions/GlobalExceptionHandler.java
@@ -1,2 +1,39 @@
-package com.drone.services.Exceptions;public class ExceptionHandler {
+package com.drone.services.Exceptions;
+
+import com.drone.services.models.Drone;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleDroneException(DroneException e) {
+        ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), errors);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleServerError(Exception e) {
+        ErrorResponse response = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
