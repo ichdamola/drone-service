3f89d3fcb4b1e52f99b924c22386c67f6dae7406
Ichdamola <adedadamola@gmail.com>
2022-08-01 09:57:53 +0100

    [UPD] install Lombok as a dependency

--- src/main/java/com/drone/services/models/Medication.java
+++ src/main/java/com/drone/services/models/Medication.java
@@ -1,8 +1,13 @@
 package com.drone.services.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
 public class Medication {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
